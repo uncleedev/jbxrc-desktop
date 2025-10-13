330f54c5ffa57beffafcc4a16501f3ea37bc40f6
uncledev <arce.jhonbrian.dev@gmail.com>
2025-10-13 16:37:59 +0800

    feat: refactor main.js and preload.mjs for improved readability and structure; enhance LayoutAdmin component with watermark

--- src/pages/admin/LayoutAdmin.tsx
+++ src/pages/admin/LayoutAdmin.tsx
@@ -4,10 +4,20 @@ import { Outlet } from "react-router-dom";
 
 export default function LayoutAdmin() {
   return (
-    <main className="h-screen bg-background min-w-screen flex gap-4 p-4">
+    <main className="h-screen bg-background min-w-screen flex gap-4 p-4 relative">
       <Sidebar />
 
-      <Card className="w-full overflow-y-auto p-4">
+      <Card className="w-full overflow-y-auto p-4 relative">
+        {/* Watermark */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
+          <span
+            className="text-gray-300 text-6xl font-bold opacity-50 select-none rotate-[-45deg]"
+            style={{ userSelect: "none" }}
+          >
+            UNCLEDEV
+          </span>
+        </div>
+
         <Outlet />
       </Card>
     </main>
